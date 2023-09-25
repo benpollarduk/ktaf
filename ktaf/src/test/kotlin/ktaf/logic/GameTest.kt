4ed0c95659932af221941c35954f732c6d7895a0
ben_pollard <ben@singerinstruments.com>
2023-09-25 17:07:40 +0100

    Added tests

--- ktaf/src/test/kotlin/ktaf/logic/GameTest.kt
+++ ktaf/src/test/kotlin/ktaf/logic/GameTest.kt
@@ -1,7 +1,10 @@
 package ktaf.logic
 
+import ktaf.assets.characters.NonPlayableCharacter
 import ktaf.assets.characters.PlayableCharacter
 import ktaf.assets.locations.Room
+import ktaf.utilities.OverworldMaker
+import ktaf.utilities.RegionMaker
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -28,5 +31,56 @@ class GameTest {
 
         // Then
         Assertions.assertTrue(result is Room)
+    }
+
+    @Test
+    fun `given no active converser when start conversation called then active converser not null`() {
+        // Given
+        val game = GameTestHelper.getBlankGame()
+        val converser = NonPlayableCharacter("", "")
+
+        // When
+        game.startConversation(converser)
+        val result = game.activeConverser
+
+        // Then
+        Assertions.assertNotNull(result)
+    }
+
+    @Test
+    fun `given active converser when end conversation called then active converser null`() {
+        // Given
+        val game = GameTestHelper.getBlankGame()
+        val converser = NonPlayableCharacter("", "")
+        game.startConversation(converser)
+
+        // When
+        game.endConversation()
+        val result = game.activeConverser
+
+        // Then
+        Assertions.assertNull(result)
+    }
+
+    @Test
+    fun `given create then not null`() {
+        // Given
+        val player = PlayableCharacter("TestPlayer", "")
+        val regionMaker = RegionMaker("TestRegion", "")
+        regionMaker[0, 0, 0] = Room("TestRoom", "")
+        val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+        val result = Game.create(
+            "",
+            "",
+            "",
+            "",
+            { overworldMaker.make() },
+            { player },
+            { EndCheckResult.notEnded },
+            { EndCheckResult.notEnded }
+        )
+
+        // Then
+        Assertions.assertNotNull(result)
     }
 }
