d5584a6bfdbb2bbc780dc2ad93affa04b7cc9617
ben_pollard <ben@singerinstruments.com>
2023-09-28 16:17:12 +0100

    Fixed error with completion condition of demo game

--- ktaf-example/src/main/kotlin/example/everglades/rooms/InnerCave.kt
+++ ktaf-example/src/main/kotlin/example/everglades/rooms/InnerCave.kt
@@ -16,7 +16,7 @@ internal class InnerCave : RoomTemplate() {
     override fun instantiate(playableCharacter: PlayableCharacter): Room {
         return Room(
             NAME,
-            DESCRIPTION,
+            "",
             listOf(
                 Exit(Direction.WEST),
                 Exit(Direction.NORTH, true)
@@ -61,8 +61,5 @@ internal class InnerCave : RoomTemplate() {
     }
     internal companion object {
         internal const val NAME = "Inner Cave"
-        private const val DESCRIPTION = "You are in a small clearing in a jungle, tightly enclosed by undergrowth. " +
-            "You have no idea how you got here. The chirps and buzzes coming from insects in the undergrowth are " +
-            "intense. There are some stone pedestals in front of you. Each has a small globe on top of it."
     }
 }
