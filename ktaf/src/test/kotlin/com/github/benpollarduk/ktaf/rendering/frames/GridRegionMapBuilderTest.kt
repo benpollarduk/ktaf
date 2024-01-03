a380b94e42d593ab5ca9611c685dce01860e33dc
ben_pollard <ben@singerinstruments.com>
2024-01-03 11:46:39 +0000

    Improved region map coverage

--- ktaf/src/test/kotlin/com/github/benpollarduk/ktaf/rendering/frames/GridRegionMapBuilderTest.kt
+++ ktaf/src/test/kotlin/com/github/benpollarduk/ktaf/rendering/frames/GridRegionMapBuilderTest.kt
@@ -10,14 +10,67 @@ import org.junit.jupiter.api.Test
 
 class GridRegionMapBuilderTest {
     @Test
-    fun `given defaults when build then string with some length returned`() {
+    fun `given single floor when build then string with some length returned`() {
         // Given
         val builder = GridRegionMapBuilder()
         val regionMaker = RegionMaker("", "")
-        regionMaker[0, 0, 0] = Room("", "", listOf(Exit(Direction.EAST)))
-        regionMaker[1, 0, 0] = Room("", "", listOf(Exit(Direction.EAST, true), Exit(Direction.WEST)))
-        regionMaker[2, 0, 0] = Room("", "", listOf(Exit(Direction.WEST, true)))
-        val region = regionMaker.make()
+        regionMaker[0, 1, 1] = Room("", "", listOf(Exit(Direction.EAST)))
+        regionMaker[1, 1, 1] = Room(
+            "",
+            "",
+            listOf(
+                Exit(Direction.EAST),
+                Exit(Direction.WEST),
+                Exit(Direction.NORTH),
+                Exit(Direction.SOUTH),
+                Exit(Direction.UP),
+                Exit(Direction.DOWN),
+            ),
+        )
+        regionMaker[2, 1, 1] = Room("", "", listOf(Exit(Direction.WEST)))
+        regionMaker[0, 0, 1] = Room("", "", listOf(Exit(Direction.SOUTH)))
+        regionMaker[0, 2, 1] = Room("", "", listOf(Exit(Direction.NORTH)))
+        regionMaker[1, 2, 0] = Room("", "", listOf(Exit(Direction.UP)))
+        regionMaker[1, 2, 2] = Room("", "", listOf(Exit(Direction.DOWN)))
+        val start = regionMaker[1, 1, 1] ?: Room("", "")
+        val region = regionMaker.make(start)
+        val gridStringBuilder = GridStringBuilder().also {
+            it.resize(Size(80, 50))
+        }
+
+        // When
+        val result = builder.build(gridStringBuilder, region, 0, 0, 80, 50).toString()
+        print(result)
+
+        // Then
+        Assertions.assertTrue(result.length > 1)
+    }
+
+    @Test
+    fun `given multiple floors with locked doors when build then string with some length returned`() {
+        // Given
+        val builder = GridRegionMapBuilder()
+        val regionMaker = RegionMaker("", "")
+        regionMaker[0, 1, 1] = Room("", "", listOf(Exit(Direction.EAST, true)))
+        regionMaker[1, 1, 1] = Room(
+            "",
+            "",
+            listOf(
+                Exit(Direction.EAST, true),
+                Exit(Direction.WEST, true),
+                Exit(Direction.NORTH, true),
+                Exit(Direction.SOUTH, true),
+                Exit(Direction.UP, true),
+                Exit(Direction.DOWN, true),
+            ),
+        )
+        regionMaker[2, 1, 1] = Room("", "", listOf(Exit(Direction.WEST, true)))
+        regionMaker[0, 0, 1] = Room("", "", listOf(Exit(Direction.SOUTH, true)))
+        regionMaker[0, 2, 1] = Room("", "", listOf(Exit(Direction.NORTH, true)))
+        regionMaker[1, 2, 0] = Room("", "", listOf(Exit(Direction.UP, true)))
+        regionMaker[1, 2, 2] = Room("", "", listOf(Exit(Direction.DOWN, true)))
+        val start = regionMaker[1, 1, 1] ?: Room("", "")
+        val region = regionMaker.make(start)
         val gridStringBuilder = GridStringBuilder().also {
             it.resize(Size(80, 50))
         }
