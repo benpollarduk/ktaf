45eb7c600380dd595170a6c81bd30554aa1b6d25
ben_pollard <ben@singerinstruments.com>
2024-01-03 11:17:52 +0000

    Improved game coverage

--- ktaf/src/test/kotlin/com/github/benpollarduk/ktaf/logic/GameTest.kt
+++ ktaf/src/test/kotlin/com/github/benpollarduk/ktaf/logic/GameTest.kt
@@ -1,5 +1,6 @@
 package com.github.benpollarduk.ktaf.logic
 
+import com.github.benpollarduk.ktaf.assets.Item
 import com.github.benpollarduk.ktaf.assets.characters.NonPlayableCharacter
 import com.github.benpollarduk.ktaf.assets.characters.PlayableCharacter
 import com.github.benpollarduk.ktaf.assets.locations.Room
@@ -75,10 +76,151 @@ class GameTest {
             player,
             overworldMaker.make(),
             { EndCheckResult.notEnded },
-            { EndCheckResult.notEnded }
+            { EndCheckResult.notEnded },
         )
 
         // Then
         Assertions.assertNotNull(result)
+    }
+
+    @Test
+    fun `given player when find interaction target then return player`() {
+        // Given
+        val player = PlayableCharacter("TestPlayer", "")
+        val regionMaker = RegionMaker("TestRegion", "")
+        regionMaker[0, 0, 0] = Room("TestRoom", "")
+        val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+        val game = Game(
+            GameInformation("", "", "", ""),
+            player,
+            overworldMaker.make(),
+            { EndCheckResult.notEnded },
+            { EndCheckResult.notEnded },
+        )
+
+        // When
+        val result = game.findInteractionTarget(player.identifier.name)
+
+        // Then
+        Assertions.assertEquals(player, result)
+    }
+
+    @Test
+    fun `given player item when find interaction target then return item`() {
+        // Given
+        val item = Item("TestItem", "")
+        val player = PlayableCharacter("TestPlayer", "")
+        player.acquireItem(item)
+        val regionMaker = RegionMaker("TestRegion", "")
+        regionMaker[0, 0, 0] = Room("TestRoom", "")
+        val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+        val game = Game(
+            GameInformation("", "", "", ""),
+            player,
+            overworldMaker.make(),
+            { EndCheckResult.notEnded },
+            { EndCheckResult.notEnded },
+        )
+
+        // When
+        val result = game.findInteractionTarget(item.identifier.name)
+
+        // Then
+        Assertions.assertEquals(item, result)
+    }
+
+    @Test
+    fun `given room item when find interaction target then return item`() {
+        // Given
+        val item = Item("TestItem", "")
+        val player = PlayableCharacter("TestPlayer", "")
+        val regionMaker = RegionMaker("TestRegion", "")
+        val room = Room("TestRoom", "")
+        room.addItem(item)
+        regionMaker[0, 0, 0] = room
+        val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+        val game = Game(
+            GameInformation("", "", "", ""),
+            player,
+            overworldMaker.make(),
+            { EndCheckResult.notEnded },
+            { EndCheckResult.notEnded },
+        )
+
+        // When
+        val result = game.findInteractionTarget(item.identifier.name)
+
+        // Then
+        Assertions.assertEquals(item, result)
+    }
+
+    @Test
+    fun `given room character when find interaction target then return character`() {
+        // Given
+        val npc = NonPlayableCharacter("TestCharacter", "")
+        val player = PlayableCharacter("TestPlayer", "")
+        val regionMaker = RegionMaker("TestRegion", "")
+        val room = Room("TestRoom", "")
+        room.addCharacter(npc)
+        regionMaker[0, 0, 0] = room
+        val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+        val game = Game(
+            GameInformation("", "", "", ""),
+            player,
+            overworldMaker.make(),
+            { EndCheckResult.notEnded },
+            { EndCheckResult.notEnded },
+        )
+
+        // When
+        val result = game.findInteractionTarget(npc.identifier.name)
+
+        // Then
+        Assertions.assertEquals(npc, result)
+    }
+
+    @Test
+    fun `given room when find interaction target then return room`() {
+        // Given
+        val player = PlayableCharacter("TestPlayer", "")
+        val regionMaker = RegionMaker("TestRegion", "")
+        val room = Room("TestRoom", "")
+        regionMaker[0, 0, 0] = room
+        val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+        val game = Game(
+            GameInformation("", "", "", ""),
+            player,
+            overworldMaker.make(),
+            { EndCheckResult.notEnded },
+            { EndCheckResult.notEnded },
+        )
+
+        // When
+        val result = game.findInteractionTarget(room.identifier.name)
+
+        // Then
+        Assertions.assertEquals(room, result)
+    }
+
+    @Test
+    fun `given display transition then no exception thrown`() {
+        // Then
+        Assertions.assertDoesNotThrow {
+            // Given
+            val player = PlayableCharacter("TestPlayer", "")
+            val regionMaker = RegionMaker("TestRegion", "")
+            regionMaker[0, 0, 0] = Room("TestRoom", "")
+            val overworldMaker = OverworldMaker("TestOverworld", "", listOf(regionMaker))
+            val game = Game(
+                GameInformation("", "", "", ""),
+                player,
+                overworldMaker.make(),
+                { EndCheckResult.notEnded },
+                { EndCheckResult.notEnded },
+            )
+
+            // When
+            game.displayTransition("Title", "Message")
+        }
     }
 }
